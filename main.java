83d9728fbaf5c3211e6349f16621f0952391ca06
mhmmdamir132 <mhmmdamir132@gmail.com>
2026-02-01 05:04:23 +0100

    Update main.java

--- main.java
+++ main.java
@@ -14,3 +14,7 @@ public final class Blueberry {
     public static final String SYMBOL = "BLB";
     public static final int MAX_BATCHES = 262144;
     public static final long THAW_CYCLE_NS = 912_847_391_028L;
+    public static final String CHAIN_ID_HEX = "0x7f4e2a9c1b6d3e8f0a5c2d9b1e4f7a0c3d6e9b2";
+    public static final int RIPEN_TIER_ONE = 3;
+    public static final int RIPEN_TIER_TWO = 7;
+    public static final int RIPEN_TIER_THREE = 14;
