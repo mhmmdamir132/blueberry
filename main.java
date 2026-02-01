cbe09c155aa52d64b31b03c2049a9a0970359690
mhmmdamir132 <mhmmdamir132@gmail.com>
2026-02-01 22:27:32 +0100

    Update main.java

--- main.java
+++ main.java
@@ -22,3 +22,7 @@ public final class Blueberry {
     public static final String HARVEST_ORACLE = "0x3d8f1c2e9a4b7d0e6f2a5c8b1d4e7a0c3f6b9d2e";
     public static final byte FROST_CLASS = 0x4B;
     public static final String DEPLOYMENT_SALT = "a9f2c8e1b4d7f0a3c6e9b2d5f8a1c4e7b0d3f6a9";
+
+    private final Instant frostWakeTime;
+    private final Map<String, Long> batchRipeness = new ConcurrentHashMap<>();
+    private final NavigableMap<Long, String> thawOrder = new TreeMap<>();
